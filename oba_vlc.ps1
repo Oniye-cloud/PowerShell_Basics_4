68f66ce56963fd2f6191a1262802d06710e895b9
Oniye-cloud <Oniyeahmad@gmail.com>
2025-10-14 12:09:53 +0100

    oba

---
+++ oba_vlc.ps1
@@ -0,0 +1,25 @@
+# $name = "oba"
+# $age = "16"
+# Write-Host "$name $age"
+
+
+
+# loop
+
+# for($i = 1; $i -le 5; $i++ )
+# {
+#     Write-Host $i
+# } 
+
+
+# ifsatement
+
+$age = 14 
+if ($age -ge 18){
+    Write-Host "you can vote"
+
+}else{
+        Write-Host "you can not vote"
+
+}
+
