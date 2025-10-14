c7737a8b58cce77e900751dbc200f015986d815f
Oniye-cloud <Oniyeahmad@gmail.com>
2025-10-14 13:07:48 +0100

    Gladys's Commit

---
+++ Gladys.ps1
@@ -0,0 +1,20 @@
+# $name = "Goodness"
+# $age = "30"
+# $school = "Oxford University"
+
+# Write-Host "My name is $name. I am $age years of age and i attend $school"
+
+
+# $alphabets = 'a' .. 'e'
+
+# foreach ($alphabet in $alphabets) {
+#     Write-Host "Alphabet is $alphabet"
+# }
+
+
+$status = "Online"
+if ($status -eq "Online") {
+    Write-Host "The server is running"
+} else {
+    Write-Host "Server is downnnn"
+}
