4dd15316f7e05f17f4d071ece3b181c7a516a4b5
Oniye-cloud <Oniyeahmad@gmail.com>
2025-10-16 14:56:19 +0100

    x-lord

---
+++ X-Lord.ps1
@@ -0,0 +1,18 @@
+# for loop
+# for ($i = 1; $i -le 5; $i++) {
+#     Write-Host $i
+# }
+
+# Foreach Loop
+# $users = @('lastborn', 'Oba', 'Gladdys')
+# foreach ($user in $users) {
+#     Write-Host "Hi $user"
+# }
+
+# While loop
+
+$count = 1
+while ($count -le 5){
+    Write-Host "count is $count"
+$count++
+}
