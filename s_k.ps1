d6f08eba7d0d873edce6e94cef5e94b3b4f42cc9
Oniye-cloud <Oniyeahmad@gmail.com>
2025-10-16 13:11:53 +0100

    fess commit

---
+++ s_k.ps1
@@ -0,0 +1,17 @@
+# $name = "jamo"
+# $age = "37"
+# $school = "kwasu"
+# Write-Host "my name is $name i am $age years old,schooling at $school"
+
+#foreach loop
+# $nums = @("2, 4, 6")
+# foreach($num in $nums){
+# Write-Host  "even num are $num"}
+
+
+$num = "20"
+if ($num -le 18){
+    Write-Host "underage"
+}else {
+    Write-Host "you are eligible"
+}
