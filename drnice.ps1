40461137650457fe7a4ed23c11f2cb9a01710384
Oniye-cloud <Oniyeahmad@gmail.com>
2025-10-16 14:21:07 +0100

    second commit

---
+++ drnice.ps1
@@ -0,0 +1,14 @@
+# $name = "Dr nice"
+# Write-Host $name
+
+
+# for ($i =10; $i -le 10; $i++){
+    
+#     Write-Host $i 
+# }
+
+$status = "Has connection"
+if($status -eq "Has connection"){
+    Write-Host "Online"
+}
+
